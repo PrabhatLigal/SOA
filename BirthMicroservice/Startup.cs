47aa5cd538f8478f6e2467b2c956a4093bb7839f
Prabhat Ligal <ligal3245@gmail.com>
2021-10-11 00:14:09 +1100

    Client MVC App added

--- BirthMicroservice/Startup.cs
+++ BirthMicroservice/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 namespace BirthMicroservice
