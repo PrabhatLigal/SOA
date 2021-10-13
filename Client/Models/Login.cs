e0fb6f3ca7edc9f45b6883aa474346d1771b00a8
Prabhat Ligal <ligal3245@gmail.com>
2021-10-13 18:24:59 +1100

    mygv covid finished

---
+++ Client/Models/Login.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Client.Models
+{
+    public class Login
+    {
+
+        [Required(ErrorMessage = "Username is required")] // make the field required
+        [Display(Name = "username")]  // Set the display name of the field
+        public string username { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [Display(Name = "password")]
+        public string password { get; set; }
+    }
+}
